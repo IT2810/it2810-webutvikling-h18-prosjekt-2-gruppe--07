fe395fd1995335922d716d331e664d89276534ad
lenatorresdal <lena-torresdal@hotmail.com>
2018-09-16 18:10:19 +0200

    Navbar only sets selected tab. #19

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,14 +7,17 @@ class Navbar extends Component {
     return (
       <div className="topnav" id="topnav">
         <div className="navbox">
-          <a href="#">Kombo 1</a>
-          <a href="#">Kombo 2</a>
-          <a href="#">Kombo 3</a>
-          <a href="#">Kombo 4</a>
+          <button onClick={() => this.props.onChange(0)}>Kombo 1</button>
+          <button onClick={() => this.props.onChange(1)}>Kombo 2</button>
+          <button onClick={() => this.props.onChange(2)}>Kombo 3</button>
+          <button onClick={() => this.props.onChange(3)}>Kombo 4</button>
         </div>
       </div>
     );
   }
+
+  
+
 }
 
 export default Navbar;
