559665bdbb42f26de070de7a25230dd168381c4b
Therese Federl <theresehfederl@gmail.com>
2018-09-17 22:13:52 +0200

    Moved the rendering of the texts from app.js to content.jsx Closes #21

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 //import Poem from './components/poem';
 
-import summer1 from './images/summer1.jpg';
-import winter1 from './images/winter1.jpg';
-import autumn1 from './images/autumn1.jpg';
-import summer2 from './images/summer2.jpg';
-import winter2 from './images/winter2.jpg';
-import autumn2 from './images/autumn2.jpg';
-import summer3 from './images/summer3.jpg';
-import winter3 from './images/winter3.jpg';
-import autumn3 from './images/autumn3.jpg';
-import summer4 from './images/summer4.jpg';
-import winter4 from './images/winter4.jpg';
-import autumn4 from './images/autumn4.jpg';
+import summer1 from "./images/summer1.jpg";
+import winter1 from "./images/winter1.jpg";
+import autumn1 from "./images/autumn1.jpg";
+import summer2 from "./images/summer2.jpg";
+import winter2 from "./images/winter2.jpg";
+import autumn2 from "./images/autumn2.jpg";
+import summer3 from "./images/summer3.jpg";
+import winter3 from "./images/winter3.jpg";
+import autumn3 from "./images/autumn3.jpg";
+import summer4 from "./images/summer4.jpg";
+import winter4 from "./images/winter4.jpg";
+import autumn4 from "./images/autumn4.jpg";
 
-
-
-import './App.css';
+import "./App.css";
 import Navbar from "./components/navbar";
 import Category from "./components/category";
-import Content from './components/content';
+import Content from "./components/content";
 
 const images = {
-  'summer': [
-    summer1, summer2, summer3, summer4,
-  ],
-  'winter': [
-    winter1, winter2, winter3, winter4,
-  ],
-  'autumn': [
-    autumn1, autumn2, autumn3, autumn4,
-  ]
+  summer: [summer1, summer2, summer3, summer4],
+  winter: [winter1, winter2, winter3, winter4],
+  autumn: [autumn1, autumn2, autumn3, autumn4]
 };
 
-
 class App extends Component {
-
   constructor(props) {
     super(props);
 
@@ -47,22 +37,20 @@ class App extends Component {
 
     this.state = {
       selectedTab: 0,
-      imageSeason: 'summer',
-      poemSeason: 'summer',
-      audioSeason: 'summer',
+      imageSeason: "summer",
+      poemSeason: "summer",
+      audioSeason: "summer",
       poems: []
-    }
+    };
   }
 
-  
-  // this method is called only once after page load 
+  // this method is called only once after page load
   async componentWillMount() {
-    let url = '/text/summer/'+this.state.selectedTab+'.json';
-    axios.get(url)
-      .then(res => {
-        let poems = res.data;
-        this.setState({ poems });
-      })
+    let url = "/text/summer/" + this.state.selectedTab + ".json";
+    axios.get(url).then(res => {
+      let poems = res.data;
+      this.setState({ poems });
+    });
   }
 
   render() {
@@ -75,23 +63,21 @@ class App extends Component {
           <Navbar onChange={this.setTab} />
         </div>
         <div className="contentImage">
-          <img src={images[this.state.imageSeason][this.state.selectedTab]} alt="Dette er bildet" />
+          <img
+            src={images[this.state.imageSeason][this.state.selectedTab]}
+            alt="Dette er bildet"
+          />
         </div>
-        <div className="contentText">
-          {this.state.poems.map(poem => (
-                <span key={poem.id}>
-                  <h1>{poem.title}</h1>
-                  <p>{poem.body}</p>
-                  <p>{this.props.data}</p>
-                </span>
-              ))}
-        </div>
-        
-        <Content audioSeason={this.state.audioSeason} tabId={this.state.selectedTab} />
+        <Content
+          poems={this.state.poems}
+          audioSeason={this.state.audioSeason}
+          tabId={this.state.selectedTab}
+        />
 
         <div className="categories">
           <h2>Image</h2>
-          <Category onChange={this.setImageSeason} /><br />
+          <Category onChange={this.setImageSeason} />
+          <br />
           <h2>Text</h2>
           <Category onChange={this.setPoemSeason} /> <br />
           <h2>Audio</h2>
@@ -104,37 +90,35 @@ class App extends Component {
   // Sets state for season (summer, winter, autumn)
   setImageSeason(season) {
     this.setState({
-      imageSeason: season,
-    })
+      imageSeason: season
+    });
   }
   setPoemSeason(season) {
     this.setState({
-      poemSeason: season,
-    })
-    let url = '/text/'+season+'/'+this.state.selectedTab+'.json';
-    axios.get(url)
-      .then(res => {
-        let poems = res.data;
-        this.setState({ poems });
-      })
+      poemSeason: season
+    });
+    let url = "/text/" + season + "/" + this.state.selectedTab + ".json";
+    axios.get(url).then(res => {
+      let poems = res.data;
+      this.setState({ poems });
+    });
   }
   setAudioSeason(season) {
     this.setState({
-      audioSeason: season,
-    })
+      audioSeason: season
+    });
   }
 
-// Sets tab index (0-3)
+  // Sets tab index (0-3)
   setTab(tab) {
     this.setState({
-      selectedTab: tab,
-    })
-    let url = '/text/'+this.state.poemSeason+'/'+tab+'.json';
-    axios.get(url)
-      .then(res => {
-        let poems = res.data;
-        this.setState({ poems });
-      })
+      selectedTab: tab
+    });
+    let url = "/text/" + this.state.poemSeason + "/" + tab + ".json";
+    axios.get(url).then(res => {
+      let poems = res.data;
+      this.setState({ poems });
+    });
   }
 }
 
--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,27 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Content extends Component {
   constructor(props) {
-      super(props)
+    super(props);
 
-      this.audioRef = React.createRef();
+    this.audioRef = React.createRef();
   }
-
 
   componentDidUpdate(prevProps) {
-      // Get audio element to re-load 
-      this.audioRef.current.pause();
-      this.audioRef.current.load();
+    // Get audio element to re-load
+    this.audioRef.current.pause();
+    this.audioRef.current.load();
   }
-// Get source url for audio
-  getSourceUrl(){
-      return "/audio/" + this.props.audioSeason + "/" + this.props.audioSeason + this.props.tabId +".mp3"
+  // Get source url for audio
+  getSourceUrl() {
+    return (
+      "/audio/" +
+      this.props.audioSeason +
+      "/" +
+      this.props.audioSeason +
+      this.props.tabId +
+      ".mp3"
+    );
   }
 
   render() {
     return (
       <div>
-          
+        <div className="contentText">
+          {this.props.poems.map(poem => (
+            <span key={poem.id}>
+              <h1>{poem.title}</h1>
+              <p>{poem.body}</p>
+              <p>{this.props.data}</p>
+            </span>
+          ))}
+        </div>
         <div className="contentAudio">
           <audio controls autoPlay ref={this.audioRef}>
             <source src={this.getSourceUrl()} type="audio/mpeg" />
@@ -29,6 +43,6 @@ export default class Content extends Component {
           </audio>
         </div>
       </div>
-    )
+    );
   }
 }
